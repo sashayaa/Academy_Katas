417e6f2bcfaa868c7062f9f19cde66b180890ac2
Aleksandra Ivanova <aleksandra98iv@gmail.com>
2022-02-11 16:55:48 +0000

    Transferring code from the main workspace

--- BowlingExercise/bowling.rb
+++ BowlingExercise/bowling.rb
@@ -1,3 +1,66 @@
-def bowling(frame)
-    1
-end
+class Bowling
+    # think of some name for max score = 10
+    def get_total_score(frames)
+        """ Gets the total score for all frames
+        Params:
+        frames (string): contains all frames separated by space
+        Returns:
+        score (integer): total score (all frames)
+        """
+        frames_list = frames.split(" ")
+        score = 0
+        frames_list.each do |frame|
+            if strike?(frame) && frames_list.last != frame
+                next_frame = frames_list[frames_list.index(frame) + 1]
+                score += 10 + get_score_frame(next_frame)
+            elsif spare?(frame) && frames_list.last != frame
+                next_frame = frames_list[frames_list.index(frame) + 1]
+                score += 10 + score_per_roll(next_frame[0])
+            else
+                score += get_score_frame(frame)
+            end
+        end
+        score
+    end
+
+    private
+
+    def score_per_roll(roll)
+        if strike?(roll) 
+            score = 10
+        else
+            score = roll.to_i
+        end
+        score
+    end
+
+    def get_score_frame(frame)
+        """ Gets score for only one frame
+        Params:
+        frame(string): contains only one frame (e.g. number, strike, spare, miss)
+        Returns:
+        score(integer): the score per one frame individually
+        """
+        if strike?(frame)
+            score = 10
+        else
+            first_roll = frame[0]  
+            second_roll = frame[1]         # Anything else besides digits will be zero: '-'.to_i -> 0
+            if second_roll == "/" 
+                score = 10
+            else
+                score = first_roll.to_i + second_roll.to_i
+            end
+        end
+        score 
+    end
+
+    def strike?(frame)
+        frame == "X"
+    end
+
+    def spare?(frame) "4/"
+        frame[1] == "/"
+    end
+
+end 
--- BowlingExercise/spec/bowling_spec.rb
+++ BowlingExercise/spec/bowling_spec.rb
@@ -1,7 +1,122 @@
-require_relative "../bowling.rb"
+require_relative '../bowling'
 
-describe "test1" do
-    it do
-        expect(1).to eq(0)
+describe Bowling do
+    bowling = Bowling.new
+    it "adds two rolls together" do
+        # Arrange
+        # bowling = described_class.new
+
+        # Act
+        result = bowling.get_total_score("45")
+
+        # Assert
+        expect(result).to eq(9)
     end
-end
+
+    it "adds two other rolls together" do
+        # Arrange
+        # bowling = described_class.new
+
+        # Act
+        result = bowling.get_total_score("32")
+
+        # Assert
+        expect(result).to eq(5)
+    end
+
+    it "adds a roll and a miss" do
+        # Arrange
+        # bowling = described_class.new
+
+        # Act
+        result = bowling.get_total_score("9-")   
+
+        # Assert
+        expect(result).to eq(9)
+    end
+
+    it "adds a roll and a spare" do
+        # Arrange
+        # bowling = described_class.new
+
+        # Act
+        result = bowling.get_total_score("5/")      
+
+        # Assert
+        expect(result).to eq(10)
+    end
+
+    it "adds a strike" do
+        # Arrange
+        # bowling = described_class.new
+
+        # Act
+        result = bowling.get_total_score("X")      
+
+        # Assert
+        expect(result).to eq(10)
+    end
+
+    it "adds two frames with first one being a number" do
+        # Arrange
+        # bowling = described_class.new
+
+        # Act
+        result = bowling.get_total_score("45 32")      
+
+        # Assert
+        expect(result).to eq(14)
+    end
+
+    it "adds two frames with first one strike and second number" do
+        # Arrange
+        # bowling = described_class.new
+
+        # Act
+        result = bowling.get_total_score("X 45")      
+
+        # Assert
+        expect(result).to eq(28)
+    end
+
+    it "adds two frames with one spare and a number" do
+        # Arrange
+        # bowling = described_class.new
+
+        # Act
+        result = bowling.get_total_score("5/ 45")      
+
+        # Assert
+        expect(result).to eq(23)
+    end
+
+    it "adds four frames spare-strike-number-miss" do
+        # Arrange
+        # bowling = described_class.new
+
+        # Act
+        result = bowling.get_total_score("5/ X 45 9-")      
+
+        # Assert
+        expect(result).to eq(57)
+    end
+
+    
+end 
+
+# Cases that will definitely work
+# "45 32"
+# "45 9-"
+# "45 X"
+# "45 5/"
+
+# Complex cases that we already handled
+# "X X" *
+# "X 45" *
+# "X 5/" *
+# "X 9-" *
+
+# Complex cases that we didn't yet handle
+# "5/ 9-" *
+# "5/ 5/" *
+# or more than 2 frames at a time
